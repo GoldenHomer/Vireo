184392b90ce5826eb3e6da37b2eccca387a3febc
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-01-22 15:37:30 -0600

    Now exporting supplemental and license files along with primary document

--- src/main/java/org/tdl/vireo/utility/export/TemplatePackagerUtility.java
+++ src/main/java/org/tdl/vireo/utility/export/TemplatePackagerUtility.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -11,13 +13,17 @@ import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.tdl.vireo.model.FieldValue;
 import org.tdl.vireo.model.Submission;
 import org.tdl.vireo.model.User;
 import org.tdl.vireo.model.repo.SubmissionRepo;
-import org.tdl.vireo.utility.export.TemplateFormatterUtility;
+import org.tdl.vireo.utility.FileIOUtility;
 
 @Service
 public class TemplatePackagerUtility {
+
+	@Autowired
+	private FileIOUtility fileIOUtility;
 	
 	@Autowired
 	private SubmissionRepo submissionRepo;
@@ -33,18 +39,34 @@ public class TemplatePackagerUtility {
 			ZipOutputStream zos = new ZipOutputStream(fos);
 			
 			for (Submission submission : findSubmissions(user)) {
-				String manifestName = "submission-" + submission.getId() + "/mets.xml";
-				zos.putNextEntry(new ZipEntry(manifestName));
+				String submissionPath = packagerName + "/submission_" + submission.getId() + "/";
+				zos.putNextEntry(new ZipEntry(submissionPath + "mets.xml"));
 				zos.write(Files.readAllBytes(buildExport(packagerName, submission).toPath()));
 				zos.closeEntry();
+				for (FieldValue fv : findAttachedFileFieldValues(submission)) {
+					zos.putNextEntry(new ZipEntry(submissionPath + fv.getFileName()));
+					zos.write(Files.readAllBytes(Paths.get(fv.getValue())));
+					zos.closeEntry();
+				}
 			}
 			zos.close();
 			fos.close();
 		} catch (IOException ioe) {
 			throw new RuntimeException("Unable to generate package", ioe);
 		}
-		
 		return pkg;
+	}
+
+	private List<FieldValue> findAttachedFileFieldValues(Submission submission) {
+		ArrayList<FieldValue> attachedFiles = new ArrayList<FieldValue>();
+		attachedFiles.addAll(submission.getSupplementalAndSourceDocumentFieldValues());
+		attachedFiles.add(submission.getPrimaryDocumentFieldValue());
+		attachedFiles.addAll(submission.getLicenseDocumentFieldValues());
+		return attachedFiles;
+	}
+
+	private List<Submission> findSubmissions(User user) {
+		return submissionRepo.batchDynamicSubmissionQuery(user.getActiveFilter(), user.getSubmissionViewColumns());
 	}
 	
 	public File buildExport(String manifestName, Submission submission) {
@@ -59,9 +81,5 @@ public class TemplatePackagerUtility {
 		}
 		
 		return manifestFile;
-	}
-	
-	private List<Submission> findSubmissions(User user) {
-		return submissionRepo.batchDynamicSubmissionQuery(user.getActiveFilter(), user.getSubmissionViewColumns());
-	}
+	}	
 }
